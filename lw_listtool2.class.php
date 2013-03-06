e4d31d2ba392dbd7b07f8bab0b446b343bbe4eea
Dr. Andreas Eckhoff <eckhoff@logicnetwork.de>
2013-03-06 11:11:09 +0100

    moved functionality to package and modified plugin to use the package

--- lw_listtool2.class.php
+++ lw_listtool2.class.php
@@ -5,27 +5,51 @@ class lw_listtool2 extends lw_plugin
     public function __construct()
     {
         parent::__construct();
-        include_once(dirname(__FILE__).'/Services/Autoloader.php');
-        $autoloader = new \lwListtool\Services\Autoloader();
-        $autoloader->setConfig($this->config);
+        spl_autoload_register(array($this, 'loader'));
     }
    
+    private function loader($className) 
+    {
+        if (strstr($className, 'LWddd')) {
+            $config = \lw_registry::getInstance()->getEntry('config');
+            $path = $this->config['plugin_path']['lw'].'lw_ddd';
+            $filename = str_replace('LWddd', $path, $className);
+        }
+        elseif (strstr($className, 'LWmvc')) {
+            $config = \lw_registry::getInstance()->getEntry('config');
+            $path = $this->config['plugin_path']['lw'].'lw_mvc';
+            $filename = str_replace('LWmvc', $path, $className);
+        }
+        else {
+            $className = str_replace("Factory", "", $className);
+            $filename = $this->config['path']['package'].$className;
+        }
+        $filename = str_replace('\\', '/', $filename).'.php';
+        
+        if (is_file($filename)) {
+            include_once($filename);
+        }
+    }    
+    
     public function deleteEntry()
     {
-        $controller = new \lwListtool\Controller\BackendController();
-        $controller->setCommand('delete');
-        $response = $controller->execute();
-        return $this->executeController($controller);
+        $response = $this->executeControllerAction('LwListtool', 'ContentoryBackend', 'deleteList', $this->request->getInt('oid'));
+        return true;
     }    
     
     public function getOutput()
     {
-        $controller = new \lwListtool\Controller\BackendController();
-        $controller->setCommand($this->request->getAlnum('pcmd'));
-        if ($this->request->getInt('oid') > 0) {
-            $controller->setConfigurationId($this->request->getInt('oid'));
+        if ($this->request->getAlnum("pcmd")) {
+            $cmd = $this->request->getAlnum("pcmd");
         }
-        $response = $controller->execute();
+        else {
+            $cmd = 'showForm';
+        }
+        $response = $this->executeControllerAction('LwListtool', 'ContentoryBackend', $cmd, $this->request->getInt('oid'));
+        
+        if ($response->getParameterByKey('reloadParent') == 1) {
+            die('<script>parent.location.reload();</script>');
+        }
         if ($response->getParameterByKey('cmd')) {
             $url = $this->config['url']['client']."admin.php?obj=content&cmd=open&oid=".$this->request->getInt('oid');
             $this->pageReload($url);
@@ -35,18 +59,35 @@ class lw_listtool2 extends lw_plugin
             $this->pageReload($url);
         }
         else {
+            if ($response->getParameterByKey('die') == 1){
+                die($response->getOutputByKey('output'));
+            }
             return $response->getOutputByKey('output');
-        }
+        } 
     }    
+    
+    protected function executeControllerAction($package, $controller, $cmd, $oid)
+    {
+        $bootstrapClass = "\\".$package."\\Controller\\Bootstrap";
+        if (class_exists($bootstrapClass, true)) {
+            $bootstrap = new $bootstrapClass();
+            $bootstrap->execute();
+        }
+        $ControllerClass = "\\".$package."\\Controller\\".$controller;
+        $Controller = new $ControllerClass($cmd, $oid);
+        return $Controller->execute();
+    }
     
     public function buildPageOutput()
     {
-        $controller = new \lwListtool\Controller\FrontendController();
-        if ($this->params['oid'] > 0) {
-            $controller->setConfigurationId($this->params['oid']);
+        if ($this->request->getAlnum("cmd")) {
+            $cmd = $this->request->getAlnum("cmd");
         }
-        $controller->init();
-        $response = $controller->execute();
+        else {
+            $cmd = 'showList';
+        }
+        $response = $this->executeControllerAction('LwListtool', 'Frontend', $cmd, $this->params['oid']);
+        
         if ($response->getParameterByKey('reloadParent') == 1) {
             die('<script>parent.location.reload();</script>');
         }
